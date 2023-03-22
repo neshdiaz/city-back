3c43bd5b703a042e3182b13a77dc3ad3a2a7a18f
neshdiaz <nesh.diaz@gmail.com>
2023-03-22 17:55:23 -0500

    Auth controller module and service creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { MoviesModule } from './movies/movies.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot(
     {isGlobal: true,}
-  ), MoviesModule],
-  controllers: [AppController, MoviesController],
-  providers: [AppService, MoviesService],
+  ), MoviesModule, AuthModule],
+  controllers: [AppController, MoviesController, AuthController],
+  providers: [AppService, MoviesService, AuthService],
 })
 export class AppModule {}
