9b38ce442fd23684c341acd346189a2339034b9a
neshdiaz <nesh.diaz@gmail.com>
2023-03-23 08:17:24 -0500

    Creating auth controller and service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService){}
+
+  @Get('getRequestToken')
+  getTopMovies(): {} {
+ 
+    return this.authService.getRequestToken();
+  }
+
+  @Post('createSession')
+  createSession(): {} {
+ 
+    return this.authService.createSession();
+  }
+
+  @Post('createList')
+  createList(): {} {
+ 
+    return 0
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private configService: ConfigService,
+  ) {}
+
+  async getRequestToken(): Promise<{}> {
+    let tmdb_key_v3 = this.configService.get('TMDB_API_KEY_V3');
+    const res = await fetch(`https://api.themoviedb.org/3/authentication/token/new?api_key=${tmdb_key_v3}`);
+    
+    return await res.json(); 
+  }
+    
+  async createSession(): Promise<{}> {
+    const token =  await this.getRequestToken()
+    console.log(token)
+
+    return token
+  }
+
+
+}
